8f5ceba59fb8c5c5f721a6b91d61c0721e7093e6
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-04-08 15:08:10 -0400

    Grep for 'admin-user'

--- token.sh
+++ token.sh
@@ -3,6 +3,6 @@
 # Your Kubernetes dashboard should be secure, but often you need the token. 
 # With admin rights to kubectl, run this script to grab the bearer token to provide to the dashboard.
 
-kubectl describe secret $(kubectl get secret | awk '/^dashboard-token-/{print $1}') | awk '$1=="token:"{print $2}' | xclip -selection clipboard -i
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') | awk '$1=="token:"{print $2}' | xclip -selection clipboard -i
 
 echo "Dashboard token is now in your clipboard."
