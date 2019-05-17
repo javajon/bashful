6ba0f8b8afd8f6acd8dc771fa26b7e245a38aad0
test-x <jjohnson@cognituum.com>
2019-05-17 15:39:27 -0400

    Updated Ansible aliasing

--- ansible-from-containers.sh
+++ ansible-from-containers.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Aliases for the common Ansible CLI commands, but instead of headaches with a local install, have the commands run from a container.
+# Prerequisite: Docker must be installed
+# Usage: cansible ansible-playbook -u playbook.yml
+# cansible = containerized ansible
 
-# Prerequisite is docker must be installed.
+# You can put these inside your dotfiles (~/.bashrc or ~/.zshrc to make handy aliases).
 # When adding these aliases to shell be sure to source them with the prefix `. `: Run `. ./[this script name].sh`
 
-alias          ansible='docker run -v "$(pwd):/app" -w "/app" -it --rm ansible/ubuntu14.04-ansible ansible --private-key=.ssh/id_rsa'
-alias ansible-playbook='docker run -v "$(pwd):/app" -w "/app" -it --rm ansible/ubuntu14.04-ansible ansible-playbook --private-key=.ssh/id_rsa'
-alias    ansible-shell='docker run -v "$(pwd):/app" -w "/app" -it --rm ansible/ubuntu14.04-ansible bash'
+alias cansible='docker run --rm -it -v $(pwd):/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.7-alpine '
